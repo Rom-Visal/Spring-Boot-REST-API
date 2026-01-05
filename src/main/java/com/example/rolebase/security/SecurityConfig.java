185eefd1dc74e19ec5d2d0e0882f14375ccfcb3d
RomVisal <salbongvi@gmail.com>
2026-01-05 20:08:02 +0700

    feat: configure spring security and jwt authentication entry point

---
+++ src/main/java/com/example/rolebase/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.rolebase.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomAuthenticationEntryPoint authenticAuthenticationEntryPoint;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/", "/register", "/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+                // Enable HTTP Basic authentication and set a custom authentication entry point
+                .httpBasic(httpBasic ->
+                        httpBasic.authenticationEntryPoint(authenticAuthenticationEntryPoint));
+
+        return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
