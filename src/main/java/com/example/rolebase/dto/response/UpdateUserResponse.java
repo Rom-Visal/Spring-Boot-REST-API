6bcf2767353c5958bcd182102ea1d7ce3b138b07
RomVisal <salbongvi@gmail.com>
2026-01-06 18:23:35 +0700

    refactor: migrate service logic to use MapStruct mappers and reduce boilerplate code

--- src/main/java/com/example/rolebase/dto/response/UpdateUserResponse.java
+++ src/main/java/com/example/rolebase/dto/response/UpdateUserResponse.java
@@ -1,11 +1,9 @@
 package com.example.rolebase.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
-@AllArgsConstructor
+@Getter
 @Builder
 public class UpdateUserResponse {
 
