f9a59b892d9f71db921e87eb924ff5729c69c640
RomVisal <salbongvi@gmail.com>
2026-01-07 14:55:31 +0700

    feat: optimize security configurations and implement database initialization

---
+++ src/main/java/com/example/rolebase/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.rolebase.service;
+
+import com.example.rolebase.entity.User;
+import com.example.rolebase.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Finding user with username {}", username);
+
+        User user = userRepository.findByUsernameWithRolesIgnoreCase(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " Not Found"));
+
+        log.info("Found user {}", user.getUsername());
+        return new UserDetailsImpl(user);
+    }
+}
